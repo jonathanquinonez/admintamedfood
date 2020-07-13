92764b37ac2eedfb8748d6503dfd09c8471a22ca
prv_jquinonez <prv_jquinonez@ath.com.co>
2020-07-12 21:29:17 -0500

    ajuste login

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,75 +1,162 @@
 <!DOCTYPE html>
-<html lang="en">
+<html class="loading" lang="en" data-textdirection="ltr">
+<!-- BEGIN: Head-->
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Bitu Administrativo</title>
-    <link rel="icon" href="https://bitumovil.com/wp-content/uploads/2016/10/iconofav.png" type="image/x-icon">
-    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
-    <link rel="stylesheet" type="text/css" href="css/fontawesome-all.min.css">
-    <link rel="stylesheet" type="text/css" href="css/iofrm-style.css">
-    <link rel="stylesheet" type="text/css" href="css/iofrm-theme4.css">
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimal-ui">
+    <meta name="description" content="Vuexy admin is super flexible, powerful, clean &amp; modern responsive bootstrap 4 admin template with unlimited possibilities.">
+    <meta name="keywords" content="admin template, Vuexy admin template, dashboard template, flat admin template, responsive admin template, web app">
+    <meta name="author" content="PIXINVENT">
+    <title>TAMED Admin Food</title>
+    <link rel="apple-touch-icon" href="{{URL::asset('assets/app-assets/images/ico/apple-icon-120.png')}}">
+    <link rel="shortcut icon" type="image/x-icon" href="{{URL::asset('assets/app-assets/images/ico/favicon.ico')}}">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:300,400,500,600" rel="stylesheet">
+
+    <!-- BEGIN: Vendor CSS-->
+    <link rel="stylesheet" type="text/css" href="{{URL::asset('assets/app-assets/vendors/css/vendors.min.css')}}">
+    <!-- END: Vendor CSS-->
+
+    <!-- BEGIN: Theme CSS-->
+    <link rel="stylesheet" type="text/css" href="{{URL::asset('assets/app-assets/css/bootstrap.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{URL::asset('assets/app-assets/css/bootstrap-extended.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{URL::asset('assets/app-assets/css/colors.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{URL::asset('assets/app-assets/css/components.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{URL::asset('assets/app-assets/css/themes/dark-layout.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{URL::asset('assets/app-assets/css/themes/semi-dark-layout.css')}}">
+
+    <!-- BEGIN: Page CSS-->
+    <link rel="stylesheet" type="text/css" href="{{URL::asset('assets/app-assets/css/core/menu/menu-types/horizontal-menu.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{URL::asset('assets/app-assets/css/core/colors/palette-gradient.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{URL::asset('assets/app-assets/css/pages/authentication.css')}}">
+    <!-- END: Page CSS-->
+
+    <!-- BEGIN: Custom CSS-->
+    <link rel="stylesheet" type="text/css" href="{{URL::asset('assets/css/style.css')}}">
+    <!-- END: Custom CSS-->
+
 </head>
-<body>
-    <div class="form-body" class="container-fluid">
-        <div class="website-logo">
-            <a href="https://www.bitu.com.co">
-                <div class="logo">
-                    <img class="logo-size" src="https://bitu.s3-us-west-2.amazonaws.com/Emails/Imagenes/Logo.png" alt="logo">
-                </div>
-            </a>
-        </div>
-        <div class="row">
-            <div class="img-holder">
-                <div class="bg"></div>
-                <div class="info-holder">
-                    <img src="" alt="">
-                </div>
+<!-- END: Head-->
+
+<!-- BEGIN: Body-->
+
+<body class="horizontal-layout horizontal-menu dark-layout 1-column  navbar-floating footer-static bg-full-screen-image  blank-page blank-page" data-open="hover" data-menu="horizontal-menu" data-col="1-column" data-layout="dark-layout">
+    <!-- BEGIN: Content-->
+    <div class="app-content content">
+        <div class="content-overlay"></div>
+        <div class="header-navbar-shadow"></div>
+        <div class="content-wrapper">
+            <div class="content-header row">
             </div>
-            <div class="form-holder">
-                <div class="form-content">
-                    <div class="form-items">
-                        <h3>Bienvenidos a Bitu</h3>
-                        <p>Usa tus credenciales para poder acceder a la plataforma de Administrador.</p>
-                        <form method="POST" action="{{ route('login') }}">
-                            {{ csrf_field() }}
-                            <input class="form-control" id="email" type="email" name="email" value="{{ old('email') }}" placeholder="Correo Electrónico" required autofocus>
-                            @if ($errors->has('email'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                            <input class="input100" type="password" id="password" name="password" placeholder="Contraseña" required>
-                            @if ($errors->has('password'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                            @endif
-                            <div class="form-button">
-                                <button id="submit" type="submit" class="ibtn">Ingresar</button>
+            <div class="content-body">
+                <section class="row flexbox-container">
+                    <div class="col-xl-8 col-11 d-flex justify-content-center">
+                        <div class="card bg-authentication rounded-0 mb-0">
+                            <div class="row m-0">
+                                <div class="col-lg-6 d-lg-block d-none text-center align-self-center px-1 py-0">
+                                    <img src="{{URL::asset('assets/app-assets/images/pages/login.png')}}" alt="branding logo">
+                                </div>
+                                <div class="col-lg-6 col-12 p-0">
+                                    <div class="card rounded-0 mb-0 px-2">
+                                        <div class="card-header pb-1">
+                                            <div class="card-title">
+                                                <h4 class="mb-0">Login</h4>
+                                            </div>
+                                        </div>
+                                        <p class="px-2">Bienvenido de nuevo, inicie sesión en su cuenta.</p>
+                                        <div class="card-content">
+                                            <div class="card-body pt-1">
+                                                <form method="POST" action="{{ route('login') }}">
+                                                    {{ csrf_field() }}
+                                                    <fieldset class="form-label-group form-group position-relative has-icon-left">
+                                                        <input type="text" class="form-control" name="email" id="user-name" placeholder="Username" required>
+                                                        @if ($errors->has('email'))
+                                                        <span class="help-block">
+                                                            <strong>{{ $errors->first('email') }}</strong>
+                                                        </span>
+                                                        @endif
+                                                        <div class="form-control-position">
+                                                            <i class="feather icon-user"></i>
+                                                        </div>
+                                                        <label for="user-name">Username</label>
+                                                    </fieldset>
+
+                                                    <fieldset class="form-label-group position-relative has-icon-left">
+                                                        <input type="password" class="form-control" name="password" id="user-password" placeholder="Password" required>
+                                                        @if ($errors->has('password'))
+                                                        <span class="help-block">
+                                                            <strong>{{ $errors->first('password') }}</strong>
+                                                        </span>
+                                                        @endif
+                                                        <div class="form-control-position">
+                                                            <i class="feather icon-lock"></i>
+                                                        </div>
+                                                        <label for="user-password">Password</label>
+                                                    </fieldset>
+                                                    <div class="form-group d-flex justify-content-between align-items-center">
+                                                        <div class="text-left">
+                                                            <fieldset class="checkbox">
+                                                                <div class="vs-checkbox-con vs-checkbox-primary">
+                                                                    <input type="checkbox">
+                                                                    <span class="vs-checkbox">
+                                                                        <span class="vs-checkbox--check">
+                                                                            <i class="vs-icon feather icon-check"></i>
+                                                                        </span>
+                                                                    </span>
+                                                                    <span class="">Remember me</span>
+                                                                </div>
+                                                            </fieldset>
+                                                        </div>
+                                                        <div class="text-right"><a href="{{route('password.request')}}" class="card-link">¿Olvidaste tu contrase&ntilde;a?</a></div>
+                                                    </div>
+                                                    <a href="auth-register.html" class="btn btn-outline-primary float-left btn-inline">Register</a>
+                                                    <button type="submit" class="btn btn-primary float-right btn-inline">Login</button>
+                                                </form>
+                                            </div>
+                                        </div>
+                                        <div class="login-footer">
+                                            <div class="divider">
+                                                <div class="divider-text">OR</div>
+                                            </div>
+                                            <div class="footer-btn d-inline">
+                                                <a href="#" class="btn btn-facebook"><span class="fa fa-facebook"></span></a>
+                                                <a href="#" class="btn btn-twitter white"><span class="fa fa-twitter"></span></a>
+                                                <a href="#" class="btn btn-google"><span class="fa fa-google"></span></a>
+                                                <a href="#" class="btn btn-github"><span class="fa fa-github-alt"></span></a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
                             </div>
-                        </form>
-                        <div class="other-links">
-                           <p><a href="{{route('password.request')}}">¿Olvidaste tu contrase&ntilde;a?</a></p>&nbsp;&nbsp;
-                        </div>
-                         <div class="other-links">
-                            <span>2020 Bitu S.A.S. Todos los derechos reservados.</span>
                         </div>
                     </div>
-                </div>
+                </section>
+
             </div>
         </div>
     </div>
-<script type="text/javascript" src="js/jquery.min.js"></script>
-<script type="text/javascript" src="js/popper.min.js"></script>
-<script type="text/javascript" src="js/bootstrap.min.js"></script>
-<script type="text/javascript" src="js/main.js"></script>
-<script>
-	window.fwSettings={
-	'widget_id':62000000390
-	};
-	!function(){if("function"!=typeof window.FreshworksWidget){var n=function(){n.q.push(arguments)};n.q=[],window.FreshworksWidget=n}}() 
-</script>
-<script type='text/javascript' src='https://widget.freshworks.com/widgets/62000000390.js' async defer></script>
+    <!-- END: Content-->
+
+
+    <!-- BEGIN: Vendor JS-->
+    <script src="{{URL::asset('assets/app-assets/vendors/js/vendors.min.js')}}"></script>
+    <!-- BEGIN Vendor JS-->
+
+    <!-- BEGIN: Page Vendor JS-->
+    <script src="{{URL::asset('assets/app-assets/vendors/js/ui/jquery.sticky.js')}}"></script>
+    <!-- END: Page Vendor JS-->
+
+    <!-- BEGIN: Theme JS-->
+    <script src="{{URL::asset('assets/app-assets/js/core/app-menu.js')}}"></script>
+    <script src="{{URL::asset('assets/app-assets/js/core/app.js')}}"></script>
+    <script src="{{URL::asset('assets/app-assets/js/scripts/components.js')}}"></script>
+    <!-- END: Theme JS-->
+
+    <!-- BEGIN: Page JS-->
+    <!-- END: Page JS-->
+
 </body>
+<!-- END: Body-->
+
 </html>
