45da7dac9c18d0906d35ae256f13d26c40b90ac7
prv_jquinonez <prv_jquinonez@ath.com.co>
2020-07-19 15:33:23 -0500

    ajuste logo

--- resources/views/admin/configuracion/verCategoriasNutricion.blade.php
+++ resources/views/admin/configuracion/verCategoriasNutricion.blade.php
@@ -86,7 +86,7 @@
     <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable" role="document">
         <div class="modal-content">
             <div class="modal-header">
-                <h4 class="modal-title" id="myModalLabel33">Crear Términos y Condiciones</h4>
+                <h4 class="modal-title" id="myModalLabel33">Crear</h4>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
