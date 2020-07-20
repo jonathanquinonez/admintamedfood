32a59c4339fbb00aff54beb6f7df7135381fbc61
prv_jquinonez <prv_jquinonez@ath.com.co>
2020-07-20 15:41:38 -0500

    Update editarDelivery.blade.php

--- resources/views/admin/Delivery/editarDelivery.blade.php
+++ resources/views/admin/Delivery/editarDelivery.blade.php
@@ -299,14 +299,4 @@
   </script>
     <script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDFZn-_2DpGEgdfnXX4gywzaGRS01HgA-U&libraries=places&callback=initMap"
     type="text/javascript"></script>
-
-    <script src="{{URL::asset('app-assets/js/scripts/datatables/datatable.js')}}"></script>
-    <script src="{{URL::asset('app-assets/vendors/js/tables/datatable/datatables.bootstrap4.min.js')}}"></script>
-    <script src="{{URL::asset('app-assets/vendors/js/tables/datatable/buttons.bootstrap.min.js')}}"></script>
-    <script src="{{URL::asset('app-assets/vendors/js/tables/datatable/buttons.print.min.js')}}"></script>
-    <script src="{{URL::asset('app-assets/vendors/js/tables/datatable/pdfmake.min.js')}}"></script>
-    <script src="{{URL::asset('app-assets/vendors/js/tables/datatable/vfs_fonts.')}}"></script>
-    <script src="{{URL::asset('app-assets/vendors/js/tables/datatable/datatables.min.js')}}"></script>
-    <script src="{{URL::asset('app-assets/vendors/js/tables/datatable/datatables.buttons.min.js')}}"></script>
-    <script src="{{URL::asset('app-assets/vendors/js/tables/datatable/buttons.html5.min.js')}}"></script>
 @endsection
